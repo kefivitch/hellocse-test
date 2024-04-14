3eca245cdac3c089656eceafb03ec254e3bd4bca
Lassad KEFI <lass3ouda@gmail.com>
2024-04-14 16:56:22 +0200

    feat: test profiles listing

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Enums\StatusEnum;
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -17,10 +18,7 @@ class ProfileTest extends TestCase
 
     public function test_store_method_creates_profile()
     {
-        // Create a dummy authenticated user
         $user = User::factory()->create();
-
-        // Mock authentication
         $this->actingAs($user);
 
         Storage::fake('public');
@@ -35,12 +33,55 @@ class ProfileTest extends TestCase
             'status' => Arr::random(StatusEnum::cases()),
         ];
 
-        // Send POST request to store endpoint with dummy data
         $response = $this->postJson('/api/v1/profiles', $profileData);
 
         // Assert that the profile was created
         $response->assertStatus(201);
         $response->assertJson(['profile' => ['image' => 'profile_images/' . $file->hashName()]]);
         Storage::disk('public')->assertExists('profile_images/' . $file->hashName());
+    }
+
+    public function test_profiles_endpoint_returns_profiles()
+    {
+        // Create profiles
+        Profile::factory()->count(3)->create(['status' => StatusEnum::Actif]);
+        Profile::factory()->count(1)->create(['status' => StatusEnum::Inactif]);
+        Profile::factory()->count(1)->create(['status' => StatusEnum::Pending]);
+
+        $response = $this->get('/api/v1/profiles');
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => ['id', 'name', 'first_name', 'image'],
+            ],
+        ]);
+
+        // Assert response data
+        $response->assertJsonCount(3, 'data');
+    }
+
+    public function test_profiles_endpoint_includes_status_for_authenticated_user()
+    {
+        // Authenticate a user
+        $user = User::factory()->create();
+        $this->actingAs($user, 'sanctum');
+
+        $profile = Profile::factory()->create(['status' => StatusEnum::Actif]);
+
+        $response = $this->get('/api/v1/profiles');
+
+        // Assert response includes status field
+        $response->assertJsonFragment(['status' => $profile->status]);
+    }
+
+    public function test_profiles_endpoint_excludes_status_for_guest()
+    {
+        Profile::factory()->create(['status' => StatusEnum::Actif]);
+
+        $response = $this->get('/api/v1/profiles');
+
+        // Assert response excludes status field
+        $response->assertJsonMissing(['status']);
     }
 }
