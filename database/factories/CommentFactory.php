add0caf7123fc723ddcfa57c8bed71e01b9654f6
Lassad KEFI <lass3ouda@gmail.com>
2024-04-14 15:24:07 +0200

    feat: create Comment Model

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'content' => $this->faker->paragraph,
+            'user_id' => function () {
+                return \App\Models\User::factory()->create()->id;
+            },
+            'profile_id' => function () {
+                return \App\Models\Profile::factory()->create()->id;
+            },
+        ];
+    }
+}
