b03120fa946db8f5893abe008eaba137f1694f28
Lassad KEFI <lass3ouda@gmail.com>
2024-04-14 17:25:03 +0200

    feat: modify and delete profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfileRequest;
 use App\Http\Resources\ProfileCollection;
 use App\Models\Profile;
 use App\Http\Requests\StoreProfileRequest;
@@ -14,9 +15,7 @@ class ProfileController extends Controller
         $profiles = Profile::whereActive()->paginate();
         return new ProfileCollection($profiles);
     }
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(StoreProfileRequest $request)
     {
         // get Authenticated user id
@@ -38,5 +37,19 @@ class ProfileController extends Controller
         $profile = Profile::create($validatedData);
 
         return response()->json(['message' => 'Profile created successfully', 'profile' => $profile], 201);
+    }
+
+    public function update(UpdateProfileRequest $request, Profile $profile)
+    {
+        $profile->update($request->validated());
+
+        return response()->json(['message' => 'Profile updated successfully', 'profile' => $profile], 200);
+    }
+
+    public function destroy(Profile $profile)
+    {
+        $profile->delete();
+
+        return response()->json(['message' => 'Profile deleted successfully'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,25 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::name('api.v1.')->group(function () {
+    
+    // Auth routes
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum')
+    ->name('user');
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    // Profile routes
+    Route::prefix('profiles')->group(function () {
+        Route::get('/', [ProfileController::class, 'index'])->name('profiles.index');
 
-Route::get('/profiles', [ProfileController::class, 'index']);
-Route::post('/profiles', [ProfileController::class, 'store'])->middleware('auth:sanctum');
-Route::post('/profiles/{profile}/comments', [CommentController::class, 'store'])->middleware('auth:sanctum');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/', [ProfileController::class, 'store'])->middleware('auth:sanctum')->name('profiles.store');
+            Route::post('/{profile}/comments', [CommentController::class, 'store'])->name('profiles.comments.store');
+            Route::put('/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
+            Route::delete('/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
+        });
+    });
+});
